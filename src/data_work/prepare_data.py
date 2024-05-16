6dd82a412a06e200cc2a85ed6770473160033f1c
2acholsk1 <p.zacholski@wp.pl>
2024-05-16 11:37:13 +0200

    work with data

--- src/data_work/prepare_data.py
+++ src/data_work/prepare_data.py
@@ -1,9 +1,23 @@
+#!/usr/bin/env python3
+"""Script used for work on data
+
+Returns:
+    bool: 1 if any error, 0 if everrything okay
+"""
 import argparse
 import pandas as pd
 import numpy as np
 
 
 def import_raw_data(year:int=2024) -> np.array:
+    """Function to import data from Basketball Reference and connect regular data with advanced
+
+    Args:
+        year (int, optional): Year of when regular season ends. Defaults to 2024.
+
+    Returns:
+        np.array: Connected regular and advanced data in specific season
+    """
         
     down_value_str = str(year-1)
     up_value_str = str(year)
@@ -15,10 +29,17 @@ def import_raw_data(year:int=2024) -> np.array:
     data_advance = pd.read_csv(importing_string_advance)
     result = pd.concat([data, data_advance], axis='columns')   
 
-    return result.drop_duplicates()
+    return result.loc[:, ~result.columns.duplicated()]
+
+
+def droping_unnecessary_data(data:pd.DataFrame) -> pd.DataFrame:
+    data = data.drop(columns=["Unnamed: 24", "Unnamed: 19"])
+    return data
 
 
 def main():
+    """Working on data: importing from raw files, eareasing NaN columns, eareasing not neccesary data
+    """
 
     parser = argparse.ArgumentParser(description="Importowanie data from different years")
     parser.add_argument("--start_year", type=int, default=1999, help="Starting year")
@@ -30,7 +51,7 @@ def main():
 
     while end_year > start_year:
         data = import_raw_data(end_year)
-        np.save("data/redacted_data/rs_"+str(end_year-1)+"_"+str(end_year)+"_full", data)
+        data.to_csv("data/redacted_data/rs_"+str(end_year-1)+"_"+str(end_year)+"_full")
         end_year -= 1
 
 if __name__ == '__main__':
--- src/data_work/workspace.py
+++ src/data_work/workspace.py
@@ -1,15 +1,8 @@
 import pandas as pd
+from src.data_work.prepare_data import import_raw_data
+from src.data_work.prepare_data import droping_unnecessary_data
 
-data = pd.read_csv("data/raw_data/rs_2005_2006_advanced.csv")
-data1 = pd.read_csv("data/raw_data/rs_2005_2006.csv")
-# data1 = data1.drop(["Rk", "Player", "Pos", "Age", "Tm", "G", "MP", "Player-additional"], axis='columns')
+data = import_raw_data(2002)
+data = droping_unnecessary_data(data)
 
-result = pd.concat([data, data1], axis='columns')
-# result = result.loc[:, ~result.columns.duplicated()]
-result = result.loc[:, ~result.columns.duplicated()]
-# print(data.head())
-# print(data1.head())
-# print(result.head())
-print(data.columns)
-print(data1.columns)
-print(result.columns)
+print(data.head())
